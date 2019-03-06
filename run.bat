fb9d0e48780f3d08137216f8b2353168904bd14a
MDe@Mingw@A8 <mde@gmx.de>
2019-03-06 19:35:48 +0100

    Unzulaessiges Zeichen in run.bat ersetzt.

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 
-@REM Startet Java-Programm dass dann HTTP-Port (Server-Port) öffnet
+@REM Startet Java-Programm dass dann HTTP-Port (Server-Port) oeffnet
 
 @set KLASSENPFAD=classes;lib/jetty-all-9.4.9.jar;lib/jackson-core-2.9.8.jar;lib\jackson-databind-2.9.8.jar;lib\jackson-annotations-2.9.8.jar
 
