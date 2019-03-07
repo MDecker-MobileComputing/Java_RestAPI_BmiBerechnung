4885e570c292e22d878dc84c4d02dddde96fd87f
MDecker-MobileComputing <mide_42b@yahoo.de>
2019-03-07 12:53:38 +0100

    Beide Run-Skripte starten die JVM jetzt mit "-server"

--- run.bat
+++ run.bat
@@ -3,5 +3,6 @@
 
 @set KLASSENPFAD=classes;lib/jetty-all-9.4.9.jar;lib/jackson-core-2.9.8.jar;lib\jackson-databind-2.9.8.jar;lib\jackson-annotations-2.9.8.jar
 
-java -cp %KLASSENPFAD% de.mide.restapi.bmi.MainServer
-@REM -cp : Classpath setzen
+java -server -cp %KLASSENPFAD% de.mide.restapi.bmi.MainServer
+@REM -cp     : Classpath setzen
+@REM -server : JVM im Server-Modus, siehe auch hier https://stackoverflow.com/a/198651/1364368 
--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@
 
 KLASSENPFAD=classes:lib/jetty-all-9.4.9.jar:lib/jackson-core-2.9.8.jar:lib/jackson-databind-2.9.8.jar:lib/jackson-annotations-2.9.8.jar
 
-java -cp $KLASSENPFAD de.mide.restapi.bmi.MainServer
-# -cp         : Classpath setzen
+java -server -cp $KLASSENPFAD de.mide.restapi.bmi.MainServer
+# -cp     : Classpath setzen
+# -server : JVM im Server-Modus, siehe auch hier https://stackoverflow.com/a/198651/1364368
