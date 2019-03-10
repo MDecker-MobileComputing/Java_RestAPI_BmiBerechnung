a7e58f4ddf28ea7cec0387932bece7dda27421f3
MDecker-MobileComputing <mide_42b@yahoo.de>
2019-03-10 15:25:19 +0100

    run.sh um auskommentiertes Beispiel fuer Verwendung neuer CmdLine-Parameter erweitert

--- run.sh
+++ run.sh
@@ -7,3 +7,6 @@ KLASSENPFAD=classes:lib/jetty-all-9.4.9.jar:lib/jackson-core-2.9.8.jar:lib/jacks
 java -server -cp $KLASSENPFAD de.mide.restapi.bmi.MainServer
 # -cp     : Classpath setzen
 # -server : JVM im Server-Modus, siehe auch hier https://stackoverflow.com/a/198651/1364368
+
+# Server an Non-Default-Port 9090 starten
+#java -server -cp $KLASSENPFAD de.mide.restapi.bmi.MainServer 9090
